1836575aacb43cc293e1d7ba868fd2dae3e0db93
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2019-05-24 16:53:44 -0700

    spark

---
+++ PCA-Spark.scala
@@ -0,0 +1,10 @@
+package sparkml
+
+// from pyspark.ml.linalg import Vectors
+// from pyspark.ml.feature import VectorAssembler
+
+object SimpleMain {
+    def main(args: Array[String]): Unit = {
+        println("Hi there!")
+    }
+}
