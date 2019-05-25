64020beab06aa6741a2857ea022532e4c53b0a34
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2019-05-25 14:49:53 -0700

    multivariate gaussian

--- PCA-Breeze/src/main/scala/Main.scala
+++ PCA-Breeze/src/main/scala/Main.scala
@@ -1,15 +1,17 @@
 import breeze.linalg._
 import breeze.numerics._
+import breeze.stats.distributions.MultivariateGaussian
 
 object Main extends App {
   val dim = 4
   val total_class = 2
   val total_sample = 100  
   val reduce_dim = 2
+
   val cov_mat = DenseMatrix.eye[Double](dim)
   var mean_vec = DenseVector.ones[Double](dim)
-  // val class_sample = MultivariateGaussian(mean_vec, cov_mat).draw()
-
-  print(MultivariateGaussian(mean_vec, cov_mat))
+  var mulVal = new MultivariateGaussian(mean_vec, cov_mat)
+  val class_sample = mulVal.draw()
+  print(class_sample)
 }
 
