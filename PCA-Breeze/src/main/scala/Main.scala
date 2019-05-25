4039e4a5184068e53930ca394e8b217c396a2b43
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2019-05-24 21:42:48 -0700

    trying multivariate

--- PCA-Breeze/src/main/scala/Main.scala
+++ PCA-Breeze/src/main/scala/Main.scala
@@ -2,7 +2,14 @@ import breeze.linalg._
 import breeze.numerics._
 
 object Main extends App {
-  println("Hello, World!")
-  
+  val dim = 4
+  val total_class = 2
+  val total_sample = 100  
+  val reduce_dim = 2
+  val cov_mat = DenseMatrix.eye[Double](dim)
+  var mean_vec = DenseVector.ones[Double](dim)
+  // val class_sample = MultivariateGaussian(mean_vec, cov_mat).draw()
+
+  print(MultivariateGaussian(mean_vec, cov_mat))
 }
 
