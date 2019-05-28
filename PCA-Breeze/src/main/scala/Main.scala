5e83b2d162a3c3617e8b89c367309c62a3ec2466
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2019-05-27 18:36:26 -0700

    input: done; mean: done

--- PCA-Breeze/src/main/scala/Main.scala
+++ PCA-Breeze/src/main/scala/Main.scala
@@ -1,17 +1,32 @@
 import breeze.linalg._
 import breeze.numerics._
 import breeze.stats.distributions.MultivariateGaussian
+import breeze.stats.mean
 
 object Main extends App {
+
+  // Generate data input
   val dim = 4
   val total_class = 2
-  val total_sample = 100  
+  val total_sample = 16  
   val reduce_dim = 2
 
+  var data = DenseMatrix.zeros[Double](total_sample*total_class, dim)	
   val cov_mat = DenseMatrix.eye[Double](dim)
-  var mean_vec = DenseVector.ones[Double](dim)
-  var mulVal = new MultivariateGaussian(mean_vec, cov_mat)
-  val class_sample = mulVal.draw()
-  print(class_sample)
+
+  for (i <- 0 until total_class) {
+    var mean_vec = DenseVector.ones[Double](dim).map(value => value * i)
+    var distr = new MultivariateGaussian(mean_vec, cov_mat)
+    for (j <- 0 until total_sample) {
+      data(j+i*total_sample, ::) := distr.sample().t
+    }
+  }  
+  
+  // Geting the mean vector
+  val mean_vec = mean(data(::, *)))  
+
+  // Computing the Scatter Matrix
+  
+
 }
 
